0a53c95888f51028b0717ac8a4c819aef8727bb8
Dtp223 <bronte@iwristlu.com>
2024-10-03 15:25:14 +0300

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://aboba.biz.ua'
+	const proxyUrl = 'https://pokuvnjadvse.click'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
